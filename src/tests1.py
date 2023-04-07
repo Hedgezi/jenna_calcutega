6d00c2e55ee096d3df074c8aa36dba98a347cb47
solomatinanataliia <128506805+solomatinanataliia@users.noreply.github.com>
2023-04-07 13:12:31 +0200

    new tests

--- src/tests1.py
+++ src/tests1.py
@@ -1,7 +1,7 @@
 import unittest
 from mathlib import evaluate
 
-class SimpleMathOperationsTestCase(unittest.TestCase):
+class TestSimpleMathLib(unittest.TestCase):
     def testA(self):
         equation = "3+2"
         assert mathlib.evaluate(equation) == 5
@@ -45,6 +45,59 @@ class SimpleMathOperationsTestCase(unittest.TestCase):
     def testK(self):
         equation = "-10-20-30-40-50"
         assert mathlib.evaluate(equation) == -150
+
+class MovDivMathLib(unittest.TestCase):
+
+    def testA(self):
+        equation = "0*2"
+        assert mathlib.evaluate(equation) == 0
+    def testB(self):
+        equation = "2/1"
+        assert mathlib.evaluate(equation) == 2
+    def testC(self):
+        equation = "2*3/6"
+        assert mathlib.evaluate(equation) == 1
+
+class OrderPowMathLib(unittest.TestCase):
+
+    def test_order_of_pow(self):
+        equation = "3^2^3"
+        assert mathlib.evaluate(equation) == 6561
+
+    def test_pow_and_mul(self):
+        equation = "2^3*4"
+        assert mathlib.evaluate(equation) == 32
+
+    def test_negative_number_root(self):
+        equation = "-9^(1/2)"
+        assert mathlib.evaluate(equation) == -3
+
+    def test_order(self):
+        equation = "2+2*20/4"
+        assert mathlib.evaluate(equation) == 12
+
+    def test_order_with_brackets(self):
+        equation = "(2+2)*20/(5-1)"
+        assert mathlib.evaluate(equation) == 20
+
+    def test_factorial_pow(self):
+        equation = "3!^2"
+        assert mathlib.evaluate(equation) == 36
+    
+    def test_negative_number_root_brackets(self):
+        equation = "(-8)^(1/3)"
+        assert mathlib.evaluate(equation) == -2
+
+    def test_negative_number_pow(self):
+        equation = "(-2)^(3)"
+        assert mathlib.evaluate(equation) == -8
+
+    def test_float_number_pow(self):
+        equation = "3.5^2"
+        assert mathlib.evaluate(equation) == 12.25
+
+    #5/0, 3.5!, (-3)!, (-4)^(1/2) non defined
+    
 
 if __name__ == '__main__':
     unittest.main()
