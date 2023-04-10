b1161cbfe0456072a3a1c84c9428e256f74852dd
omomot <garripotter737@gmail.com>
2023-04-10 18:08:41 +0200

    Almost

--- Mathlib/treeClass.py
+++ Mathlib/treeClass.py
@@ -15,14 +15,16 @@ class Stack():
         self.arr.append(data)
 
     def pop(self):
-        if self.size() == 0:
-            return "empty"
-        return self.arr.pop(-1)
+        try:
+            return self.arr.pop(-1)
+        except:
+            pass
     
     def top(self):
-        if self.size() == 0:
-            return "empty"
-        return self.arr[-1]
+        try:
+            return self.arr[-1]
+        except:
+            pass
 
     def size(self):
         return len(self.arr)
@@ -31,26 +33,69 @@ class EqTree:
     def __init__(self, postfix_expr):
         self.expr = postfix_expr
         self.root = None
-        self.buildTree(self.expr)
+        self.build_tree(self.expr)
 
-    def is_operator(element):
+    def is_operator(self, element):
         opertrs = ["!", "+", "-", "*", "/", "%", "^"]
         if element in opertrs:
             return True
         return False
 
     def build_tree(self, expr):
-        tree_stack = []
+        tree_stack = Stack()
         self.root = EqNode(expr[-1]) 
-        tree_stack.append(self.root)
+        tree_stack.push(self.root)
         
+        for i in "".join(reversed(expr[:-1])):
+            curr_node = tree_stack.top()
+            if not curr_node.right:
+            # if right node of current node is NULL
+                temp_node = EqNode(i)
+                curr_node.right = temp_node
+                if self.is_operator(i):
+                    tree_stack.push(temp_node)
+            else: # if left node of current node is NULL
+                temp_node = EqNode(i)
+                curr_node.left = temp_node
+                # if no child node of current node is NULL
+                tree_stack.pop() # pop current from stack
+                if self.is_operator(i):
+                    tree_stack.push(temp_node)
+    
+    def evaluate_tree(self, curr_node = None):
+        if curr_node is None: # Initializing start of recursion 
+            curr_node = self.root
+
+        if curr_node is None: # Checking if tree is empty
+            return 0
+        
+        if not self.is_operator(curr_node.data):
+            return curr_node.data
+
+    
+    
+    
+    
+    
+    
+    
+    
+    def inorder(self, head):
+      # inorder traversal of expression tree
+      # inorder traversal = > left, root, right
+        if head.left:
+            self.inorder(head.left)
+        print(head.data, end=" ")
+        if head.right:
+            self.inorder(head.right)
+    def infixExp(self):
+      # inorder traversal of expression tree give infix expression
+        self.inorder(self.root)
+        print()
     
 
 
-
-
-def check():
-    root = EqNode(1)
-    root.printNode()
-
-check()
+if __name__ == "__main__":
+   postfixExp = "ab+ef*g*-"
+   et = EqTree(postfixExp)
+   et.infixExp()
