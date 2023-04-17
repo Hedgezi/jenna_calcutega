29e98f5d974f25c291802a68a311c52d42706d20
solomatinanataliia <128506805+solomatinanataliia@users.noreply.github.com>
2023-04-17 18:12:13 +0200

    new more tests

--- src/tests.py
+++ src/tests.py
@@ -134,7 +134,105 @@ class OrderPowMathLib(unittest.TestCase):
     def test_zero_factorial(self):
         equation = "0!"
         assert Expression(equation).result == 1
+
+    def test_remainder_of_division(self):
+        equation = "5%2"
+        assert Expression(equation).result == 1
+
+    def test_remaider_of_division_by_zero(self):
+        equation = "5%0"
+        self.assertRaises(ZeroDivisionError, Expression(equation).result)
+
+    def test_remainder_of_division_by_one(self):
+        equation = "5%1"
+        assert Expression(equation).result == 0
+    def test_remainder_of_division_by_negative_number_without_brackets(self):
+        equation = "5%-2"
+        assert Expression(equation).result == -1
+
+    def test_remainder_of_division_by_negative_number_with_brackets(self):
+        equation = "5%(-2)"
+        assert Expression(equation).result == -1
+
+
+class AdvancedTestsMathLib(unittest.TestCase):
+
+    def test_factorial_and_arithmetic_operations(self):
+        equation = "4!-5!/5+3!"
+        assert Expression(equation).result == 6
+
+    def test_factorial_and_arithmetic_operations_with_brackets(self):
+        equation = "(4!-5!)/(2+3!)"
+        assert Expression(equation).result == -12
+
+    def test_factorial_and_arithmetic_operations_with_brackets_and_negative_numbers(self):
+        equation = "(-4!-5!)/(2+3!)"
+        assert Expression(equation).result == -18
+
+    def test_float_brackerts(self):
+        equation = "(1.2*6.3/2*(0.5+0.75))"
+        assert Expression(equation).result == 4.725
+
+    def test_float_brackerts_2(self):
+        equation = "((2.7^2 - 1.5*3)*7.1)"
+        assert Expression(equation).result == 19.809
+
+    def test_brackets_not_close(self):
+        equation = "(2+3-5"
+        self.assertRaises(ValueError, Expression(equation).result)
+
+    def test_brackets_not_open(self):
+        equation = "2+3)-5"
+        self.assertRaises(ValueError, Expression(equation).result)
+
+    def test_arithmetics_operation_near(self):
+        equation = "2+*3-^4"
+        self.assertRaises(ValueError, Expression(equation).result)
+
+    def test_arithmetics_operation_near_2(self):
+        equation = "2+3-4*"
+        self.assertRaises(ValueError, Expression(equation).result)
+
+    def test_factorial_float_brackets(self):
+        equation = "(3+3.5)!"
+        self.assertRaises(ValueError, Expression(equation).result)
+
+class BracketsTestMathLib(unittest.TestCase):
+    def test_brackets_1(self):
+        equation = "((((3+7)/(3-1))*(6!-5!))-5^2"
+        assert Expression(equation).result == 2975
+
+    def test_brackets_2(self):
+        equation = "(((3+7)))"
+        assert Expression(equation).result == 10
+
+    def test_brackets_3(self):
+        equation = "(2+3)!"
+        assert Expression(equation).result == 120
+
+    def test_brackets_4(self):
+        equation = "(2+3.5^2-8.25)!/(32%11)"
+        assert Expression(equation).result == 72
+
+    def test_brackets_5(self):
+        equation = "(5+1)*(2+3)"
+        assert Expression(equation).result == 30
+
+    def test_brackets_6(self):
+        equation = "((5+7)*(3-4)*((3^2)-4^(1/2))/10"
+        assert Expression(equation).result == -8.4
+
+    def test_brackets_7(self):  
+        equation = "(2+3)*(2+3)!"
+        assert Expression(equation).result == 600
     
+    def test_big_numbers(self):
+        equation = "1000000000000000000+3246864214690521467"
+        assert Expression(equation).result == 4246864214690521467
+    
+    def test_big_numbers_2(self):    
+        equation = "1000000000000000000*10000"
+        assert Expression(equation).result == 10000000000000000000000
 
 if __name__ == '__main__':
     unittest.main()
